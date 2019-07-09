c27eea159257c9c491f645cfc489ec1ba001b9fd
Eric Diaz <ericdiaz@pursuit.org>
2019-07-09 13:25:07 -0400

    created test directory and added mock test

---
+++ app/src/test/java/AppUnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppUnitTest {
+    @Test
+    public void baseTest() {
+        String baseUrl = "https://raw.githubusercontent.com";
+        Assert.assertNull(baseUrl);
+    }
+
+}
